77c9c7a07067ee7fc2d409740854e1d7d26f1a29
Ethan <ethan@ewuddy.com>
2020-03-02 11:48:48 -0500

    Update description-email.php

--- includes/description-email.php
+++ includes/description-email.php
@@ -1,10 +1,73 @@
 <h2 class="featured-in text-uppercase text-center mt-3"><span><?= T('Featured In'); ?></span></h2>
 <div class="logos mt-5">
-    <img class="img-fluid" src="../resources/images/logos.jpg">
-</div>
+Thanks for opening our emails</div>
 <div class="three-benefites three-benefited__count-four d-flex justify-content-between mt-5">
-<h1>Thanks for opening our E-mail! look around</h1>
+    
 </div>
+
+<div class="magic-grid mt-5">
+
+
+</div>
+
+
+<div class="magic-grid mt-5">
+
+    <div class="d-flex flex-row">
+        <div>
+
+            <img class="img-fluid" src="../resources/images/100-natural-img.jpg">
+        </div>
+    </div>
+
+    <div class="d-flex flex-row">
+        <div>
+    </div>
+
+    <div class="d-flex flex-row">
+        
+    </div>
+
+    <div class="d-flex flex-row">
+       
+    <div class="d-flex flex-row">
+       </div>
+
+<div class="red-arrow-next-line">
+    <img src="../resources/images/red_arrow_next_line.svg" alt>
+</div>
+
+
+
+
+<div class="daily_treatment_section mt-5">
+    <div class="row">
+      
+    </div>
+    <div class="row">
+        
+    </div>
+</div>
+
+
+<div class="guaranteed-satisfaction mt-5">
+    <div class="text-center">
+        <img src="../resources/images/guarantee-seal.png" alt>
+    </div>
+   
+
+
+
+<div class="container">
+    <div class="accordion" id="accordionExample">
+        <div class="card">
+            <div class="card-header" id="headingOne">
+                <h2 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                            data-target="#collapseOne" aria-expanded="false"
+                            aria-controls="collapseOne"><?= T('FAQ'); ?></button>
+                </h2>
+            </div>
 
             <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
                 <div class="card-body">
